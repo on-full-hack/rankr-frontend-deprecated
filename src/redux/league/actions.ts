e2775c7041e9f12454a6aa4fac526854bd7609f8
Lukasz <lukaszwypchal@gmail.com>
2019-06-23 03:28:52 +0200

    Join to league

--- src/redux/league/actions.ts
+++ src/redux/league/actions.ts
@@ -1,6 +1,6 @@
 import {createAction} from 'typesafe-actions';
 import * as types from './types';
-import {League} from '../../API';
+import {League, LeagueDetails} from '../../API';
 
 export const createLeague = createAction(
   types.CREATE_LEAGUE,
@@ -17,4 +17,14 @@ export const fetchLeagues = createAction(types.FETCH_LEAGUES);
 export const fetchLeaguesSuccess = createAction(
   types.FETCH_LEAGUES_SUCCESS,
   resolve => (leagues: League[]) => resolve({leagues})
+);
+
+export const fetchLeagueDetails = createAction(
+  types.FETCH_LEAGUE_DETAILS,
+  resolve => (id: number) => resolve({id})
+);
+
+export const fetchLeagueDetailsSuccess = createAction(
+  types.FETCH_LEAGUE_DETAILS_SUCCESS,
+  resolve => (leagueDetails: LeagueDetails) => resolve({leagueDetails})
 );
